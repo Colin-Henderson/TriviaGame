f93a17802b3844aa77bf686a1cccf4d57bd8c959
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2017-12-01 15:14:59 -0500

    Finished everything except making background image dynamic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,14 +29,18 @@ function displayResults() {
 	var q4 = document.forms.form.q4.value;
 	var q5 = document.forms.form.q5.value;
 	var q6 = document.forms.form.q6.value;
+	var q7 = document.forms.form.q7.value;
+	var q8 = document.forms.form.q8.value;
+	var q9 = document.forms.form.q9.value;
+	var q10 = document.forms.form.q10.value;
 
 // The q variables correspond with the  answer variables [i]
-	var questions = [q1, q2, q3, q4, q5, q6];
+	var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
-	var answers = ["c", "d", "d", "d", "d", "d"];
+	var answers = ["c", "d", "d", "d", "d", "d", "b", "b", "b", "c"];
 
 	var points = 0;
-	var total = 6;
+	var total = 10;
 
 	for (var i = 0; i < total; i++) {
 		if (questions[i] === answers[i]) {
@@ -44,11 +48,23 @@ function displayResults() {
 		}
 	}
 	// Displays the resuslts in the results div
-	document.getElementById("Results").innerHTML = "Your scored " + points + " out of 6 points";
+
+	document.getElementById("Results").innerHTML = "Your scored " + points + " out of 10 points!";
+	document.getElementById("dustin").style.display="block";
+	topPage();
+
 };
 
 
-var timeLeft = 5;
+// // Was going to try and create a function that would create a picture for different scores(happy dustin for above 3 and then sad dustin for under 3 but could not get it to work)
+// function choosePic() {
+// 	if (points >== 3){
+// 		document.getElementById("dustin").style.display="block";
+// 	}
+// };
+
+
+var timeLeft = 45;
 var timerID = null; 
 
 
@@ -64,7 +80,6 @@ function startQuiz() {
 function countdown() {
 	if(timeLeft == -1) {
 		clearTimeout(timerID);
-		// Havent made this function yet but we will
 		displayResults();
 	} else {
 		document.getElementById("timer").innerHTML = timeLeft + " second(s) remaining";
@@ -72,6 +87,10 @@ function countdown() {
 	}
 };
 
+// This function will scroll us to the top of the page (going to use that so you will see your score immediately after countdown)
+function topPage() {
+	document.documentElement.scrollTop = 0;
+};
 
 
 
